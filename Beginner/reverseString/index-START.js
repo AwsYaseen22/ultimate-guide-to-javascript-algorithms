aeeb94095bc725b250b5d7b98fbd3219076e7b50
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-10-19 00:37:18 +0300

    solve all reverse string with test

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,9 +3,36 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-// Chaining in-built methods
-function reverseString(text) {
-  return text.split("").reverse().join("");
-}
+module.exports = {
+  // Chaining in-built methods
+  reverseString1: function (text) {
+    return text.split("").reverse().join("");
+  },
 
-module.exports = reverseString;
+  // Using a For-loop
+  reverseString2: function (text) {
+    let result = "";
+    for (let i = text.length - 1; i >= 0; i--) {
+      result += text[i];
+    }
+    return result;
+  },
+
+  // Using The Recursive Way
+  reverseString3: function (text) {
+    if (text === "") {
+      return "";
+    } else {
+      return this.reverseString3(text.substr(1)) + text[0];
+    }
+  },
+
+  // Using .reduce()
+  reverseString4: function (text) {
+    return text.split("").reduce((acc, cur) => (cur += acc), "");
+  },
+};
+
+// console.log(module.exports.reverseString4("hello"));
+
+// module.exports = reverseString;
--- Beginner/reverseString/test.js
+++ Beginner/reverseString/test.js
@@ -1,20 +1,35 @@
-const reverseString = require('./index-START')
+let ob = require("./index-START");
 
-test('reverseString is a function', () => {
-  expect(typeof reverseString).toEqual('function');
-});
+for (const key in ob) {
+  test(`${ob[key]} is a function`, () => {
+    expect(typeof ob[key]).toEqual("function");
+  });
 
-test('reverses a string of text', () => {
-  expect(reverseString('aeiou')).toEqual('uoiea');
-});
+  test("reverses a string of text", () => {
+    expect(ob[key]("aeiou")).toEqual("uoiea");
+  });
 
-test('reverses a string containing numbers', () => {
-  expect(reverseString('123456789')).toEqual('987654321');
-});
+  test("reverses a string containing numbers", () => {
+    expect(ob[key]("123456789")).toEqual("987654321");
+  });
 
-test('reverses a string containing mixed case characters', () => {
-  expect(reverseString('AsDfGhJkL')).toEqual('LkJhGfDsA');
-});
+  test("reverses a string containing mixed case characters", () => {
+    expect(ob[key]("AsDfGhJkL")).toEqual("LkJhGfDsA");
+  });
+}
 
+// test('reverseString is a function', () => {
+//   expect(typeof reverseString).toEqual('function');
+// });
 
+// test('reverses a string of text', () => {
+//   expect(reverseString('aeiou')).toEqual('uoiea');
+// });
 
+// test('reverses a string containing numbers', () => {
+//   expect(reverseString('123456789')).toEqual('987654321');
+// });
+
+// test('reverses a string containing mixed case characters', () => {
+//   expect(reverseString('AsDfGhJkL')).toEqual('LkJhGfDsA');
+// });
