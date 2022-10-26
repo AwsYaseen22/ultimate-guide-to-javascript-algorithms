3b1dff226796f8befc8d106596c2fbc301a6ebb8
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-10-26 23:25:18 +0300

    solve vowelsCounter approach 2

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,17 +3,20 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
-function vowelsCounter(text) {
-  let vowels = ["a", "e", "i", "o", "u"];
-  let counter = 0;
-  for (let letter of text.toLowerCase()) {
-    if (vowels.includes(letter)) {
-      counter++;
-    }
-  }
-  return counter;
-}
+// function vowelsCounter(text) {
+//   let vowels = ["a", "e", "i", "o", "u"];
+//   let counter = 0;
+//   for (let letter of text.toLowerCase()) {
+//     if (vowels.includes(letter)) {
+//       counter++;
+//     }
+//   }
+//   return counter;
+// }
 
-// console.log(vowelsCounter("Aws Yaseen Ahmed"));
+function vowelsCounter(text) {
+  let counter = text.match(/[aeiou]/gi);
+  return counter ? counter.length : 0;
+}
 
 module.exports = vowelsCounter;
