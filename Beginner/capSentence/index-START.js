ff7b9ba1e459e28c4abfa03dd93bbe25e3c12be5
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-10-28 21:01:35 +0300

    solve capSentence approach 2

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -4,19 +4,24 @@ return the equivalent of the sentence when capitalised. E.g
   capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
 */
 
+// function capSentence(text) {
+//   let arr = text.toLowerCase().split(" ");
+//   let capsArr = [];
+//   arr.forEach((w) => {
+//     capsArr.push(w[0].toUpperCase() + w.slice(1));
+//   });
+//   return capsArr.join(" ");
+// }
+
 function capSentence(text) {
   let arr = text.toLowerCase().split(" ");
-  let capsArr = [];
-  arr.forEach((w) => {
-    capsArr.push(w[0].toUpperCase() + w.slice(1));
+  let capsArr = arr.map((w) => {
+    return w[0].toUpperCase() + w.slice(1);
   });
   return capsArr.join(" ");
 }
 
-// function capSentence(text) {
-
-// }
-
+f;
 console.log(capSentence("the tales of scotch!"));
 
 module.exports = capSentence;
