b20f342aebf7e20190605623322ae1f8afd35671
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-10-29 23:43:07 +0300

    solve palindromeChecker with 3 approaches with test and JSBench.me

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -3,13 +3,31 @@ Given a string of text, return true or false indicating whether or not the text 
 e.g palindromeChecker('racecar') // will return true
 */
 
-
-
-
 function palindromeChecker(text) {
-    v// Code goes here
+  return text.toLowerCase().split("").reverse().join("") === text;
 }
 
+// function palindromeChecker(text) {
+//   let arr = text.toLowerCase().split("");
+//   let res = arr.every((e, i) => e === arr[arr.length - 1 - i]);
+//   return res;
+// }
 
+// function palindromeChecker(text) {
+//   //   let arr = text.toLowerCase().split("");
+//   //   let res = arr
+//   //     .slice(0, Math.floor(arr.length / 2))
+//   //     .every((e, i) => e === arr[arr.length - 1 - i]);
+//   //   return res;
+//   var textLen = text.length;
+//   for (var i = 0; i < textLen / 2; i++) {
+//     if (text[i] !== text[textLen - 1 - i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// console.log(palindromeChecker("racecar"));
 
 module.exports = palindromeChecker;
