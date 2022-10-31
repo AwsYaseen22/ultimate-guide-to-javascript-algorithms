aa2d3f442de860a870518ae0ab6ffe62a3b00d8e
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-10-31 23:31:02 +0300

    solve longestWord with two approach

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -1,13 +1,29 @@
 /* CHALLENGE
 Given a string of text, write an algorithm that returns the text received in a reversed format. 
 E.g reverseString('algorithms') // should return 'smhtirogla'
+Amendment => return longest word 
 */
 
-
+// function longestWord(text) {
+//   text = text.split(" ");
+//   let longest = text[0];
+//   for (let i = 1; i < text.length; i++) {
+//     if (text[i].length > longest.length) {
+//       longest = text[i];
+//     }
+//   }
+//   return longest;
+// }
 
 function longestWord(text) {
-    // Code goes here
+  return text.split(" ").reduce((acc, cur) => {
+    if (cur.length > acc.length) {
+      acc = cur;
+    }
+    return acc;
+  }, "");
 }
 
+console.log(longestWord("Top Shelf Web Development Training on Scotch"));
 
-module.exports = longestWord
+module.exports = longestWord;
