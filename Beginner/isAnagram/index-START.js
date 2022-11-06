3db9a83e38c1bcd077de55971cb8b017ccb8c627
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-11-06 22:56:37 +0300

    solve isAnagram second approach

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -5,17 +5,48 @@ don't. E.g
     isAnagram('silent', 'listen') // should return true
 */
 
-const sanitizeString = function (str) {
-  return str
-    .toLowerCase()
-    .replace(/[^a-z]/g, "")
-    .split("")
-    .sort()
-    .join("");
-};
+// const sanitizeString = function (str) {
+//   return str
+//     .toLowerCase()
+//     .replace(/[^a-z]/g, "")
+//     .split("")
+//     .sort()
+//     .join("");
+// };
+
+// function isAnagram(stringA, stringB) {
+//   return sanitizeString(stringA) === sanitizeString(stringB);
+// }
+
+// console.log(isAnagram("silent", "listen"));
+
+// second approach with character mapping
+
+function createCharMap(text) {
+  let charMap = {};
+  for (const char of text) {
+    if (charMap[char] !== undefined) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  return charMap;
+}
 
 function isAnagram(stringA, stringB) {
-  return sanitizeString(stringA) === sanitizeString(stringB);
+  if (stringA.length === stringB.length) {
+    let mapA = createCharMap(stringA);
+    let mapB = createCharMap(stringB);
+    for (const char in mapA) {
+      if (mapA[char] !== mapB[char]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(isAnagram("silent", "listen"));
