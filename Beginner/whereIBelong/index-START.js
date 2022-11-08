3896b9be010e920d60e187a08f9d1b4ad5666ad5
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-11-08 23:38:34 +0300

    solve Where Do I Belong

--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -7,13 +7,46 @@ whereIBelong([1,2,3,4], 1.5) // should return 1 because it is greater
 than 1(index 0), but less than 2(index 1).
 */
 
+// Using a For-loop to find the immediate larger value's position
+// function whereIBelong(arr, num) {
+//   arr.sort((a, b) => a - b);
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] >= num) {
+//       return i;
+//     }
+//   }
+//   return arr.length;
+// }
 
+// Using a For-loop to determine the number of smaller values (FASTER)
+// function whereIBelong(arr, num) {
+//   let counter = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (num > arr[i]) {
+//       counter++;
+//     }
+//   }
+//   return counter;
+// }
 
+// Using a while loop to count the smaller values
+// function whereIBelong(arr, num) {
+//   arr.sort((a, b) => a - b);
+//   let counter = 0;
+//   while (num > arr[counter]) {
+//     counter++;
+//   }
+//   return counter;
+// }
+
+// Finding the index of the value directly
 function whereIBelong(arr, num) {
-   // Code goes here
-
+  arr.push(num);
+  arr.sort((a, b) => a - b);
+  return arr.indexOf(num);
 }
 
+// console.log(whereIBelong([1, 2, 3, 4], 1.5));
+// should return 1 because it is greater than 1(index 0), but less than 2(index 1).;
 
-
-module.exports = whereIBelong
+module.exports = whereIBelong;
