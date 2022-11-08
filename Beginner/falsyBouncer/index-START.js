615b84501f655a16fdce27e14522f370cfe6c04f
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-11-08 23:15:25 +0300

    solve falsyBouncer

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -5,10 +5,23 @@ and return an array of only truthy values.
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
-
+// Using a for…of loop
 function falsyBouncer(array) {
-    // Code goes here
+  let result = [];
+  for (const val of array) {
+    if (val) {
+      result.push(val);
+    }
+  }
+  return result;
 }
 
+// Using .filter() (FASTER a little bit)
+// function falsyBouncer(array) {
+//   return array.filter((e) => Boolean(e));
+// }
 
-module.exports = falsyBouncer
+// console.log(falsyBouncer([1, 0, null, "", 5]));
+// should return [1,5]
+
+module.exports = falsyBouncer;
