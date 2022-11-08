c4d8145fb19298dc3359e1f50a4a644799a64ce0
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-11-08 23:03:32 +0300

    solve Combining Arrays Without Duplicates

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,18 +4,50 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+// Using Sets
+// function mergeArrays(...arrays) {
+//   let jointArray = [];
+
+//   arrays.forEach((array) => {
+//     jointArray = [...jointArray, ...array];
+//   });
+
+//   return [...new Set([...jointArray])];
+// }
+
+// Using .filter()
+// function mergeArrays(...arrays) {
+//   let jointArray = [];
+
+//   arrays.forEach((array) => {
+//     jointArray = [...jointArray, ...array];
+//   });
+//   //   console.log(jointArray);
+//   return jointArray.filter((e, i) => {
+//     // console.log(jointArray.indexOf(e), i);
+//     if (jointArray.indexOf(e) === i) {
+//       return e;
+//     }
+//   });
+// }
+
+// Using .reduce() (FASTER)
 function mergeArrays(...arrays) {
+  let jointArray = [];
 
-    let jointArray = []
-    
-    arrays.forEach(array => {
-        jointArray = [...jointArray, ...array]
-    });
+  arrays.forEach((array) => {
+    jointArray = [...jointArray, ...array];
+  });
 
-    return [...new Set([...jointArray])]
-
-    
+  return jointArray.reduce((acc, cur) => {
+    if (!acc.includes(cur)) {
+      acc.push(cur);
+    }
+    return acc;
+  }, []);
 }
 
+// console.log(mergeArrays([1, 2, 3, 3, 3], [1, 4, 5, 2]));
+// should return [1,2,3,4,5]
 
-module.exports = mergeArrays
+module.exports = mergeArrays;
